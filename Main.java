5a9bbfdeb0d17cb7585486300ff696f77620620f
ohnoruu <rubydao007@gmail.com>
2024-10-07 11:18:36 -0700

    more funny shapes

--- Main.java
+++ Main.java
@@ -4,6 +4,25 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    //Problem 1
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the radius of the circle: ");
+    double cirRadius = sc.nextDouble();
+    Circle circle = new Circle(cirRadius);
+    double circumference = circle.getCircumference();
+    double cirArea = circle.getArea();
+    System.out.println("A circle with a radius of " + cirRadius + " has a circumference of " + circumference + " and an area of " + cirArea);
+
+    //Problem 2 
+    System.out.println("Enter number of sides: ");
+    int numSides = sc.nextInt();
+    System.out.println("Enter the side length: ");
+    double sidelength = sc.nextDouble();
+    RegularPolygon poly1 = new RegularPolygon(numSides, sidelength);
+    double poly1area = poly1.getArea();
+    System.out.println("Area with " + numSides + " sides: " + poly1area + "\nIncrementing number of sides by two");
+    RegularPolygon poly2 = new RegularPolygon(numSides + 2, sidelength);
+    double poly2area = poly2.getArea();
+    System.out.println("Area with " + (numSides + 2) + " sides: " + poly2area);
   }
 }
